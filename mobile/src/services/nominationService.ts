d7b18f8b6f0ee8217a19e44e7a1f8a30ce204ef7
Suhel Shaikh <ssuhel915@gmail.com>
2026-06-01 00:52:35 +0530

    Nomination withdraw

---
+++ mobile/src/services/nominationService.ts
@@ -0,0 +1,19 @@
+import api from './api';
+
+export const nominationService = {
+  getMyForElection: async (electionId: number) => {
+    const response = await api.get('/nominations/my', {
+      params: {
+        election_id: electionId,
+        page: 1,
+        per_page: 1,
+      },
+    });
+    return response.data.items?.[0] || null;
+  },
+
+  withdraw: async (nominationId: number) => {
+    const response = await api.delete(`/nominations/${nominationId}`);
+    return response.data;
+  },
+};
